51030fd3e2489679ad3ad35051c41ecbbd179608
Andrea Jemison <andreadjemison@yahoo.com>
2019-05-01 21:51:45 -0400

    updated styling

--- server.js
+++ server.js
@@ -12,19 +12,17 @@ const app = express()
 const methodOverride = require('method-override')
 // logger
 const logger = require('morgan')
+
 // imported apis & schemas from each model
 const herbApi = require('./api/herbs.js')
 const oilApi = require('./api/oils.js')
 const supApi = require('./api/sups.js')
-const ailApi = require('./api/ailments.js')
+
 
 //= =====================
 // MIDDLEWARE
 //= =====================
-// body parser(req.body) for classic encoding if false
 app.use(express.urlencoded({ extended: true }))
-
-// body parser using JSON
 app.use(express.json())
 
 // for use in forms since those are only POST & GET
@@ -54,53 +52,57 @@ app.get('/', (req, res) => {
 // list index of all herbs
 app.get('/herbs', (req, res) => {
     herbApi.allHerbs()
-    .then(herbs => {
-        console.log(herbApi.allHerbs(req.body))
-        res.render('herbs/index', { herbs })
-    })
+        .then(herbs => {
+            console.log(herbApi.allHerbs(req.body))
+            res.render('herbs/index', { herbs })
+        })
 })
 
 // this gets the new herb form page to render
 app.get('/herbs/new', (req, res) => {
-        res.render('herbs/new')
-    })
+    res.render('herbs/new')
+})
+
 // gets the new herb form inputs and adds it to herb index
 app.post('/herbs', (req, res) => {
     herbApi.newHerb(req.body)
-    .then(() => {
-        res.redirect('/herbs')
-    })
+        .then(() => {
+            res.redirect('/herbs')
+        })
 })
 
 // show single herb
 app.get('/herbs/:id', (req, res) => {
-    herbApi.oneHerb(req.params.id).then(herb => {
-        res.render('herbs/show', { herb })
-    })
+    herbApi.oneHerb(req.params.id)
+        .then(herb => {
+            res.render('herbs/show', { herb })
+        })
 })
 
 //delete single herb
 app.delete('/herbs/:id', (req, res) => {
-    herbApi.deleteHerb(req.params.id).then(() => {
-        res.redirect('/herbs')
-    })
+    herbApi.deleteHerb(req.params.id)
+        .then(() => {
+            res.redirect('/herbs')
+        })
 })
 
 //delete all herbs
 app.delete('/herbs', (req, res) => {
-    herbApi.deleteAllHerbs(req.params.id).then(() => {
-        res.redirect('/herbs')
-    })
+    herbApi.deleteAllHerbs(req.params.id)
+        .then(() => {
+            res.redirect('/herbs')
+        })
 })
 
-//update single herb
+//grabs specific herb page to edit
 app.get('/herbs/:id/edit', (req, res) => {
     herbApi.oneHerb(req.params.id)
         .then(herb => {
             res.render('herbs/edit', { herb })
         })
 })
-
+//replace single herb
 app.put('/herbs/:id', (req, res) => {
     herbApi.updateHerb(req.params.id, req.body)
         .then(() => {
@@ -108,9 +110,9 @@ app.put('/herbs/:id', (req, res) => {
         })
 })
 
-//= =====================================================
+//=======================================================
 // OILS
-//= =====================================================
+//=======================================================
 
 // list/ index of all oils
 app.get('/oils', (req, res) => {
@@ -120,30 +122,33 @@ app.get('/oils', (req, res) => {
     })
 })
 
-// create new oil
+// grabs new oil page
 app.get('/oils/new', (req, res) => {
     res.render('oils/new')
 })
 
+// create new oil
 app.post('/oils', (req, res) => {
     oilApi.newOil(req.body)
-    .then(() => {
-        res.redirect('/oils')
-    })
+        .then(() => {
+            res.redirect('/oils')
+        })
 })
 
 // show single oil
 app.get('/oils/:id', (req, res) => {
-    oilApi.oneOil(req.params.id).then(oil => {
-        res.render('oils/show', { oil })
-    })
+    oilApi.oneOil(req.params.id)
+        .then(oil => {
+            res.render('oils/show', { oil })
+        })
 })
 
 //delete single oil
 app.delete('/oils/:id', (req, res) => {
-    oilApi.deleteOil(req.params.id).then(() => {
-        res.redirect('/oils')
-    })
+    oilApi.deleteOil(req.params.id)
+        .then(() => {
+            res.redirect('/oils')
+        })
 })
 //delete all oils
 app.delete('/oils', (req, res) => {
@@ -152,7 +157,7 @@ app.delete('/oils', (req, res) => {
     })
 })
 
-//update single oil
+//grabs single oil page to edit
 app.get('/oils/:id/edit', (req, res) => {
     oilApi.oneOil(req.params.id)
         .then(oil => {
@@ -160,6 +165,7 @@ app.get('/oils/:id/edit', (req, res) => {
         })
 })
 
+//update single oil
 app.put('/oils/:id', (req, res) => {
     oilApi.updateOil(req.params.id, req.body)
         .then(() => {
@@ -167,11 +173,11 @@ app.put('/oils/:id', (req, res) => {
         })
 })
 
-//= =====================================================
+//=======================================================
 // SUPPLEMENTS
-//= =====================================================
+//=======================================================
 
-// list/ index of all supplements
+// list index of all supplements
 app.get('/sups', (req, res) => {
     supApi.allSups().then(sups => {
         console.log(supApi.allSups())
@@ -179,15 +185,17 @@ app.get('/sups', (req, res) => {
     })
 })
 
-// create new supplement
+// grabs new supplement page
 app.get('/sups/new', (req, res) => {
     res.render('sups/new')
 })
+
+// create new supplement
 app.post('/sups', (req, res) => {
     supApi.newSup(req.body)
-    .then(() => {
-        res.redirect('/sups')
-    })
+        .then(() => {
+            res.redirect('/sups')
+        })
 })
 
 // show single supplement
@@ -209,14 +217,16 @@ app.delete('/sups', (req, res) => {
         res.redirect('/sups')
     })
 })
-//update single supplement
+
+//grabs single supplement page to edit
 app.get('/sups/:id/edit', (req, res) => {
     supApi.oneSup(req.params.id)
-    .then(sup => {
-        res.render('sups/edit', { sup })
-    })
+        .then(sup => {
+            res.render('sups/edit', { sup })
+        })
 })
 
+//update single supplement
 app.put('/sups/:id', (req, res) => {
     supApi.updateSup(req.params.id, req.body)
         .then(() => {
@@ -224,81 +234,8 @@ app.put('/sups/:id', (req, res) => {
         })
 })
 
-//= =====================================================
-// AILMENTS
-//= =====================================================
-
-// list/ index of all ailments
-app.get('/ailments', (req, res) => {
-    ailApi.allAilments().then(ailments => {
-        // console.log(ailApi.allAilments())
-        res.render('ailments/index', { ailments })
-    })
-})
-
-// create new ailment
-app.get('/ailments/new', (req, res) => {
-    res.render('ailments/new')
-})
-
-app.post('/ailments', (req, res) => {
-    ailApi.newAilment(req.body).then(() => {
-        res.redirect('/ailments')
-    })
-})
-
-// show single ailment
-app.get('/ailments/:id', (req, res) => {
-    ailApi.oneAilment(req.params.id).then(ailment => {
-        res.render('ailments/show', { ailment })
-    })
-})
-
-//delete single ailment
-app.delete('/ailments/:id', (req, res) => {
-    ailApi.deleteAilment(req.params.id).then(() => {
-        res.redirect('/ailments')
-    })
-})
-//delete all ailment
-app.delete('/ailments', (req, res) => {
-    ailApi.deleteAllAilments(req.params.id).then(() => {
-        res.redirect('/ailments')
-    })
-})
-
-//update single ailment
-app.get('/ailments/:id/edit', (req, res) => {
-    ailApi.oneAilment(req.params.id)
-    .then(ailment => {
-        res.render('ailments/edit', { ailment })
-    })
-})
-
-app.put('/ailments/:id', (req, res) => {
-    ailApi.updateAilment(req.params.id, req.body)
-        .then(() => {
-            res.redirect(`/ailments/${req.params.id}`)
-        })
-})
 
 let PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log('working')
 })
-
-//   console.log(herbApi.allHerbs())
-//   for(let i=0; i<oil.ailmentName.length; i++){
-//     if (ailment.ailmentName === oil.ailmentName){
-//       return oilCollection.find()
-//     }
-//   }
-//   for(let i=0; i<sup.ailmentName.length; i++){
-//     if (ailment.ailmentName === sup.ailmentName){
-//       return supplementCollection.find()
-//     }
-//   }
-// console.log(herbApi.allHerbs(ailApi.oneAilment(`${id}`)))
-// console.log(herbApi.allHerbs().length)
-// console.log(herbApi.allHerbs())
-// console.log(herbApi.allHerbs(req.body))
